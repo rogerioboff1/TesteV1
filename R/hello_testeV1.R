53248cfcbfd9e8f0f8330b1f481e632935f9d308
rogerioboff1 <rogerio.boff@conectt.com.br>
2019-05-20 14:01:02 -0300

    dfxf

---
+++ R/hello_testeV1.R
@@ -0,0 +1,21 @@
+
+#' @title Teste V1
+#'
+#' @description Descricao do teste V1
+#'
+#' @param
+#'
+#' @return
+#'
+#' @examples
+#'
+#' @export
+#'
+
+
+# https://www.analyticsvidhya.com/blog/2017/03/create-packages-r-cran-github/
+hello <- function() {
+  print("Hello, world! Rogerio teste")
+}
+
+retorna8 <- function() return(8)
